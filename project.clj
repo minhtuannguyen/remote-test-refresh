92cac8ae03a69ea14d6d35c9fac05b737d143b39
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2016-12-23 14:41:44 +0100

    update deps

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
 
   :dependencies [[org.clojure/tools.namespace "0.2.11" :exclusions [org.clojure/clojure]]
                  [clj-ssh "0.5.14"]
-                 [org.clojure/core.async "0.2.391"]]
+                 [org.clojure/core.async "0.2.395"]]
 
   :profiles {:uberjar {:aot :all}
              :test    {:resource-paths ["test-resources"]}
              :dev     {:dependencies [[pjstadig/humane-test-output "0.8.1"]]
-                       :plugins      [[lein-cljfmt "0.5.5"]
-                                      [lein-cloverage "1.0.6"]
+                       :plugins      [[lein-cljfmt "0.5.6"]
+                                      [lein-cloverage "1.0.9"]
                                       [jonase/eastwood "0.2.3"]
-                                      [lein-kibit "0.1.2"]]}})
+                                      [lein-kibit "0.1.3"]]}})
